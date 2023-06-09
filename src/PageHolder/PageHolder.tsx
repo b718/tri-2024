f091c83bfb336355ded2193f5c39937775cb7ad9
whiffy <bryanzhao168@yahoo.ca>
2023-06-08 22:44:44 -0700

    Added temporary video for now!

--- src/PageHolder/PageHolder.tsx
+++ src/PageHolder/PageHolder.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./PageHolder.css";
 import { Center, Image, Text } from "@mantine/core";
+import { AspectRatio } from "@mantine/core";
+
 import headImage from "../Images/head-image-transformed.png";
 
 const PageHolder = () => {
@@ -10,13 +12,22 @@ const PageHolder = () => {
         <Image width={180} src={headImage} className="page-holder-div-image" />
       </Center>
 
+      <AspectRatio ratio={16 / 9} className="page-holder-aspect-ratio-frame">
+        <iframe
+          src="https://www.youtube.com/embed/Dorf8i6lCuk"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </AspectRatio>
       <div className="page-holder-text-section">
         <h1 style={{ textAlign: "center", fontSize: "5rem" }}>
-          Something is coming ...
+          TRI2024 is coming!
         </h1>
         <Center>
           <Text fz="sm" style={{ marginTop: "1rem" }}>
-            We're working on it! Check back soon for an update!
+            Check back soon for an update!
           </Text>
         </Center>
       </div>
