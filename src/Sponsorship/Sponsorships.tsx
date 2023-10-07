2bbbf8ff4862201ccf30ade6155840580c6b8a6a
whiffy <bryanzhao168@yahoo.ca>
2023-10-07 16:42:22 -0700

    Added new sponsor.

--- src/Sponsorship/Sponsorships.tsx
+++ src/Sponsorship/Sponsorships.tsx
@@ -4,9 +4,11 @@ import { NavBarContext } from "../App";
 import { Flex, Image, Text, Center } from "@mantine/core";
 import googleImage from "../Images/Sponsorships/brainhealth.jpg";
 import comingSoon from "../Images/null_image.png";
+import oticonImage from "../Images/Oticon_Logo_LCT_160px_rgb_pos.png";
 const Sponsorships = () => {
   const sponsorshipRef = useContext(NavBarContext);
   const imageWidth = 240;
+  const oticonLink = "https://www.oticon.ca/hearing-aid-users";
   return (
     <div
       className="sponsorships-div-section"
@@ -14,21 +16,23 @@ const Sponsorships = () => {
     >
       <Text className="sponsorships-main-header">Sponsorship</Text>
       <div className="sponsorships-main-flex">
-        {/* <Flex
+        <Flex
           justify="center"
           align="center"
           wrap="wrap"
           direction="row"
           gap={"4rem"}
         >
+          <a href={oticonLink} target="_blank">
+            <Image width={imageWidth} fit="contain" src={oticonImage} />
+          </a>
+          {/* <Image width={imageWidth} fit="contain" src={comingSoon} />
           <Image width={imageWidth} fit="contain" src={comingSoon} />
           <Image width={imageWidth} fit="contain" src={comingSoon} />
           <Image width={imageWidth} fit="contain" src={comingSoon} />
-          <Image width={imageWidth} fit="contain" src={comingSoon} />
-          <Image width={imageWidth} fit="contain" src={comingSoon} />
-          <Image width={imageWidth} fit="contain" src={comingSoon} />
-        </Flex> */}
-        <Flex justify="center" align="center">
+          <Image width={imageWidth} fit="contain" src={comingSoon} /> */}
+        </Flex>
+        {/* <Flex justify="center" align="center">
           <Text
             size={"3rem"}
             style={{ fontStyle: "italic" }}
@@ -36,7 +40,7 @@ const Sponsorships = () => {
           >
             COMING SOON
           </Text>
-        </Flex>
+        </Flex> */}
       </div>
     </div>
   );
