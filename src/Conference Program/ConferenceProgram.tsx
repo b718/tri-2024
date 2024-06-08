6700ca81f33df49dba1da373b60a9b94cedc952f
whiffy <bryanzhao168@yahoo.ca>
2024-06-07 23:33:01 -0700

    jun. 7th, 2024: - added new schedule.

--- src/Conference Program/ConferenceProgram.tsx
+++ src/Conference Program/ConferenceProgram.tsx
@@ -85,8 +85,8 @@ const ConferenceProgram = () => {
             </Text>
             <button className="conference-program-button">
               <a
-                href="Speaker Program June 2.pdf"
-                download="Detailed-Presenter-Schedule-Updated June 2nd, 2024"
+                href="Speaker Program June 7.pdf"
+                download="Detailed-Presenter-Schedule-Updated June 7nd, 2024"
               >
                 Download Schedule
               </a>
